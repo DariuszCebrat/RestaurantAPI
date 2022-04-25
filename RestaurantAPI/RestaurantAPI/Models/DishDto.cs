f3db2b5db8f8418e0cb93711a90b6b210e01b271
holygwent <39002523+holygwent@users.noreply.github.com>
2022-04-25 20:48:35 +0200

    changing dishDto

--- RestaurantAPI/RestaurantAPI/Models/DishDto.cs
+++ RestaurantAPI/RestaurantAPI/Models/DishDto.cs
@@ -2,7 +2,7 @@
 {
     public class DishDto
     {
-      //  public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
